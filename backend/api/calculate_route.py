f8117253ac968806464446918b7d9fc6b62c5f52
mariankijewski <mariankijewski@outlook.com>
2024-09-28 17:24:12 +0200

    now the api gets name of the street instead of coordinates

--- backend/api/calculate_route.py
+++ backend/api/calculate_route.py
@@ -1,7 +1,27 @@
 import osmnx as ox
 from flask import Flask, request, jsonify
+import pandas as pd
 
 app = Flask(__name__)
+
+def get_coordinates(address):
+    csv_file = '../data/interpreter.csv'
+    # Read the CSV file
+    df = pd.read_csv(csv_file, delimiter='\t')
+    
+    # Split the address into street and house number
+    street, house_number = address.rsplit(' ', 1)
+    
+    # Filter the DataFrame for the matching street and house number
+    result = df[(df['addr:street'] == street) & (df['addr:housenumber'] == house_number)]
+    
+    # If a match is found, return the coordinates
+    if not result.empty:
+        lat = result['@lat'].values[0]
+        lon = result['@lon'].values[0]
+        return (lat, lon)
+    else:
+        return None  # or raise an exception
 
 def node_ids_to_coords(route, G):
     coords = []
@@ -23,18 +43,20 @@ def calculate_route():
         return jsonify({"error": "Start and end coordinates must be provided"}), 400
 
     try:
-        Ax, Ay = data['start']
-        Bx, By = data['end']
+        A = data['start']
+        B = data['end']
     except (ValueError, TypeError):
         return jsonify({"error": "Invalid start or end coordinates"}), 400
 
-    G = ox.load_graphml("./krakow3200m.graphml")    
+    Ax, Ay = get_coordinates(A)
+    Bx, By = get_coordinates(B)
+    G = ox.load_graphml("../data/krakow3200m.graphml")    
     orig = ox.distance.nearest_nodes(G, Ay, Ax)
     dest = ox.distance.nearest_nodes(G, By, Bx)
     
     route = ox.shortest_path(G, orig, dest, weight="length")
     route = node_ids_to_coords(route, G)
-    
+    print(route)
     return jsonify(route)
 
 if __name__ == '__main__':
