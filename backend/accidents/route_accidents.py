b06687e18f4cfc72757cbbf0bc4d293787841ad1
FabianRapp <fa.rapp@web.de>
2024-09-29 07:17:26 +0200

    FIX(route_accidents)

--- backend/accidents/route_accidents.py
+++ backend/accidents/route_accidents.py
@@ -11,10 +11,11 @@ def coords_match(crash_point, point1, point2, accuracy=0.0009):#about 100m accur
     extrema_x[1] = max(point1[0], point2[0])
     extrema_y[0] = min(point1[1], point2[1])
     extrema_y[1] = max(point1[1], point2[1])
-    if extrema_y[0] - accuracy > crash_point[1] or extrema_y[1] + accuracy < crash_point[1]:
+
+    if extrema_x[0] - accuracy > crash_point[0] or extrema_x[1] + accuracy < crash_point[0]:
         return False
 
-    if extrema_x[0] - accuracy > crash_point[0] or extrema_x[1] + accuracy < crash_point[1]:
+    if extrema_y[0] - accuracy > crash_point[1] or extrema_y[1] + accuracy < crash_point[1]:
         return False
 
     return True
@@ -35,10 +36,11 @@ def get_route_accidents(route):
             for i in range(len(route) - 1):
                 point1 = route[i]
                 point2 = route[i + 1]
-                #print(point1)
-                #print(point2)
                 if coords_match(crash_vals, point1, point2):
-                    print(f"match: crash: {crash_vals}; p1: {point1}; p2: {point2}")
+                    print(f"1: {point1}")
+                    print(f"2: {point2}")
+                    print(f"crash: {crash_vals}")
+
                     accidents[key_crashes] = value_crashes
     return accidents
 
@@ -322,3 +324,9 @@ if __name__ == '__main__':
     score2 = score_accident(dummy_route2)
     #print(f"result 2:{result2}")
     print(f"result 2 score:{score2}")
+
+    dummy_route3 = [(50.0600246, 19.9715055), (50.0598741, 19.9722713), (50.0593773, 19.9750134), (50.0593656, 19.9756849), (50.0593925, 19.9756943), (50.0596807, 19.9757852), (50.0598101, 19.9757556), (50.0598533, 19.9759545), (50.0600113, 19.9768042), (50.0610334, 19.9792379), (50.0611225, 19.9794458), (50.0616589, 19.9805467), (50.0619815, 19.9813726), (50.0625419, 19.9825387), (50.0633178, 19.9841272), (50.0642784, 19.9877577), (50.0642967, 19.9878469), (50.0643181, 19.9879519), (50.0644959, 19.988852), (50.0645139, 19.9889433), (50.0645604, 19.9890254), (50.0645891, 19.9892045), (50.0646214, 19.9894068), (50.0646483, 19.9895633), (50.0646478, 19.9897204), (50.0647771, 19.9908134), (50.065221, 19.9934197), (50.0653111, 19.9939309), (50.0657553, 19.9965483), (50.065785, 19.9967383), (50.0658088, 19.9968747), (50.0658369, 19.9970461), (50.0672685, 20.0036855), (50.0674006, 20.0038554), (50.0676441, 20.0039153), (50.0680682, 20.0040289), (50.0699899, 20.004351), (50.070154, 20.0034494), (50.0702836, 20.0027453), (50.0698326, 20.0027528), (50.0697637, 20.0027219), (50.0691646, 20.0024815), (50.0694383, 20.0007742), (50.069576, 20.0000052), (50.0695415, 19.9993989)]
+    result3 = get_route_accidents(dummy_route3)
+    score3 = score_accident(dummy_route3)
+    #print(f"result 2:{result2}")
+    print(f"result 3 score:{score3}")
